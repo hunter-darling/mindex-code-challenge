3255463ac797b7d35fa44ea053c2ad39e1ce3d41
mindex <dev@mindex.com>
2025-09-30 11:20:59 -0400

    populate DB with test Compensation data

--- src/main/java/com/mindex/challenge/DataBootstrap.java
+++ src/main/java/com/mindex/challenge/DataBootstrap.java
@@ -1,7 +1,9 @@
 package com.mindex.challenge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +14,47 @@ import java.io.InputStream;
 
 @Component
 public class DataBootstrap {
-    private static final String DATASTORE_LOCATION = "/static/employee_database.json";
+  private static final String DATASTORE_LOCATION = "/static/employee_database.json";
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+  private static final String COMPENSATION_DATASTORE_LOCATION = "/static/compensation_database.json";
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
-    @PostConstruct
-    public void init() {
-        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
+  @Autowired
+  private CompensationRepository compensationRepository;
 
-        Employee[] employees = null;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-        try {
-            employees = objectMapper.readValue(inputStream, Employee[].class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  @PostConstruct
+  public void init() {
+    InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
 
-        for (Employee employee : employees) {
-            employeeRepository.insert(employee);
-        }
+    Employee[] employees = null;
+
+    try {
+      employees = objectMapper.readValue(inputStream, Employee[].class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+
+    for (Employee employee : employees) {
+      employeeRepository.insert(employee);
+    }
+
+    inputStream = this.getClass().getResourceAsStream(COMPENSATION_DATASTORE_LOCATION);
+
+    Compensation[] compensations = null;
+
+    try {
+      compensations = objectMapper.readValue(inputStream, Compensation[].class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    for (Compensation compensation : compensations) {
+      compensationRepository.insert(compensation);
+    }
+  }
 }
